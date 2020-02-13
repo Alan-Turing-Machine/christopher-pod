0655410ba35a55a695b7c82a408fd57dcd687dd9
Bryce Payne <brycedpayne@gmail.com>
2020-02-13 16:55:50 -0600

    added comments

--- js/wacky-casino.js
+++ js/wacky-casino.js
@@ -1,52 +1,56 @@
 (function () {
 
     'use strict'
-
+//***************************************   Welcome Message ************************************************************
     console.log('Welcome to the WACKY CASINO');
 
+
+//***************************************   Coin flip game  ************************************************************
     function headsOrTails() {
 
-        var again = true;
+        var again = true;  //  sets the play again var to true for do/while
 
         do {
 
-            var coinBin = Math.floor(Math.random() * 2);
-            var coin;
+            var coinBin = Math.floor(Math.random() * 2);     // random bit
+            var coin;                                           // coin var declaration
 
-            var userInBit = 2;
 
             do {
+                var userInBit = 2;                                  // sets user input in bin to 2
 
-                var userInput = prompt('Heads or Tails?').toString().toLowerCase();
+                var userInput = prompt('Heads or Tails?').toString().toLowerCase();   //gets user input of H/T
 
                 if (userInput === 'heads') {
-                    userInBit--;
+                    userInBit--;                                // sets user decision to 1 for while stat and comp
                     break;
                 } else if (userInput === 'tails') {
-                    userInBit -= 2;
+                    userInBit -= 2;                             // sets user decision to 0 for while stat and comp
                     break;
                 }
-            } while (userInBit > 1);
+            } while (userInBit > 1);                            //checks cond of input to verify proper input
 
-            if (coinBin === 1) {
+            if (coinBin === 1) {                                //sets number value to string 'heads'
                 coin = 'HEADS';
-            } else {
+            } else {                                            //sets number value to string 'tails'
                 coin = 'TAILS';
             }
 
 
-            if (userInput === coinBin) {
+            if (userInBit === coinBin) {                        // comparator for win/loss cond.
                 alert('The coin was: ' + coin + ' Winner, Winner, Chicken dinner!!!');
             } else {
                 alert(" I'm sorry it's..." + coin)
             }
 
-            again = confirm('Play again?')
+            again = confirm('Play again?')                      // checks if user want to play again.
 
         } while (again === true);
 
     }
 
+
+//***************************************  Console Init     ************************************************************
 
     function gameInit() {
         var toPlay = confirm('SHALL WE PAY A GAME?');
