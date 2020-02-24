73bfb423288e28206b404c15f797d8d002630697
Carlos Teller <carlos.z.teller@gmail.com>
2020-02-24 16:12:47 -0600

    Added the functionality for rock, paper scissors, guess which hand and pick a number games; added mushroom cloud image; modified class names in the CSS file for play again

--- js/wacky-casino.js
+++ js/wacky-casino.js
@@ -80,9 +80,11 @@
                         goodbye(name);
                         break;
                     case 1:
+                        document.querySelector('#games-list').removeAttribute('class');
                         war(name);
                         break;
                     case 2:
+                        document.querySelector('#games-list').removeAttribute('class');
                         rockPaperScissors(name);
                         break;
                     case 3:
@@ -90,9 +92,11 @@
                         headsOrTails(name);
                         break;
                     case 4:
+                        document.querySelector('#games-list').removeAttribute('class');
                         leftOrRight(name);
                         break;
                     case 5:
+                        document.querySelector('#games-list').removeAttribute('class');
                         oneToTen(name);
                         break;
                     case 6:
@@ -126,7 +130,95 @@
 
     // ******************** Rock, Paper, Scissors ********************
     function rockPaperScissors(name) {
+        /*
+        * Add active class to "Rock, Paper, Scissors"
+        * make sure input is empty and focus on input
+        */
+        document.querySelector('#rock-paper-scissors').setAttribute('class', 'active');
+        document.querySelector('#rps-option').value = "";
+        document.querySelector('.rps-results').innerHTML = "";
+        document.querySelector('.rps-play-again').classList.remove('active');
+        document.querySelector('#rps-again').value = "";
+        document.querySelector('#rps-option').focus();
 
+        document.querySelector('#rps-option').addEventListener('keyup', function(e) {
+            var rps_option = document.querySelector('#rps-option').value.toLowerCase();
+
+            if(e.key === 'Enter') {
+                e.preventDefault();
+
+                if(rps_option === 'rock' || rps_option === 'paper' || rps_option === 'scissors') {
+                    rps(rps_option, name);
+                } else {
+                    document.querySelector('#rps-option').value = "";
+                    document.querySelector('#rps-option').focus();
+                }
+            }
+        });
+
+        function rps(rps, name) {
+            var opponentRPS = Math.floor(Math.random() * 2);
+
+            switch(opponentRPS) {
+                case 0:
+                    opponentRPS = 'rock';
+                    break;
+                case 1:
+                    opponentRPS = 'paper';
+                    break;
+                case 2:
+                    opponentRPS = 'scissors';
+            }
+
+            switch(name.toLowerCase()) {
+                case 'clark':
+                    break;
+                case 'wopr':
+                    break;
+                default:
+                    if(rps === opponentRPS) {
+                        document.querySelector('.rps-results').innerHTML = "You chose: " + rps + ". Your opponent chose: " + opponentRPS + ". It's a tie!";
+                    } else if(rps === 'rock' && opponentRPS === 'paper') {
+                        document.querySelector('.rps-results').innerHTML = "You chose: " + rps + ". Your opponent chose: " + opponentRPS + ". You lose!";
+                    } else if(rps === 'rock' && opponentRPS === 'scissors') {
+                        document.querySelector('.rps-results').innerHTML = "You chose: " + rps + ". Your opponent chose: " + opponentRPS + ". You win!";
+                    } else if(rps === 'paper' && opponentRPS === 'rock') {
+                        document.querySelector('.rps-results').innerHTML = "You chose: " + rps + ". Your opponent chose: " + opponentRPS + ". You win!";
+                    } else if(rps === 'paper' && opponentRPS === 'scissors') {
+                        document.querySelector('.rps-results').innerHTML = "You chose: " + rps + ". Your opponent chose: " + opponentRPS + ". You lose!";
+                    } else if(rps === 'scissors' && opponentRPS === 'rock') {
+                        document.querySelector('.rps-results').innerHTML = "You chose: " + rps + ". Your opponent chose: " + opponentRPS + ". You lose!";
+                    } else if(rps === 'scissors' && opponentRPS === 'paper') {
+                        document.querySelector('.rps-results').innerHTML = "You chose: " + rps + ". Your opponent chose: " + opponentRPS + ". You win!";
+                    }
+
+                    document.querySelector('.rps-play-again').classList.add('active');
+                    document.querySelector('#rps-again').value = "";
+                    document.querySelector('#rps-again').focus();
+
+                    document.querySelector('#rps-again').addEventListener('keyup', function(e) {
+                        var play_again = document.querySelector('#rps-again').value.toLowerCase();
+
+                        if(e.key === 'Enter') {
+                            e.preventDefault();
+
+                            if(play_again === 'y' || play_again === 'n') {
+                                if(play_again === 'y') {
+                                    document.querySelector('.rps-results').innerHTML = "";
+                                    document.querySelector('.rps-play-again').classList.remove('active');
+                                    rockPaperScissors(name);
+                                } else {
+                                    document.querySelector('#rock-paper-scissors').removeAttribute('class');
+                                    showGamesList(name);
+                                }
+                            }   else {
+                                document.querySelector('#rps-again').value = "";
+                                document.querySelector('#rps-again').focus();
+                            }
+                        }
+                    });
+            }
+        }
     }
 
     // ******************** Coin Toss / Heads or Tails ********************
@@ -138,7 +230,7 @@
         document.querySelector('#coin-toss').setAttribute('class', 'active');
         document.querySelector('#coin-option').value = "";
         document.querySelector('.coin-toss-results').innerHTML = "";
-        document.querySelector('.play-again').classList.remove('active');
+        document.querySelector('.coin-play-again').classList.remove('active');
         document.querySelector('#coin-toss-again').value = "";
         document.querySelector('#coin-option').focus();
 
@@ -148,7 +240,7 @@
             if(e.key === 'Enter') {
                 e.preventDefault();
 
-                if(coin_option.toLowerCase() === 'heads' || coin_option.toLowerCase() === 'tails') {
+                if(coin_option === 'heads' || coin_option === 'tails') {
                     flipCoin(coin_option, name);
                 } else {
                     document.querySelector('#coin-option').value = "";
@@ -178,11 +270,11 @@
                         document.querySelector('.coin-toss-results').innerHTML = "Sorry, it's " + coinFlip + ". You lose.";
                     }
 
-                    document.querySelector('.play-again').classList.add('active');
+                    document.querySelector('.coin-play-again').classList.add('active');
                     document.querySelector('#coin-toss-again').value = "";
                     document.querySelector('#coin-toss-again').focus();
 
-                    var play_again = document.querySelector('#coin-toss-again').addEventListener('keyup', function(e) {
+                    document.querySelector('#coin-toss-again').addEventListener('keyup', function(e) {
                         var play_again = document.querySelector('#coin-toss-again').value.toLowerCase();
 
                         if(e.key === 'Enter') {
@@ -191,7 +283,7 @@
                             if(play_again === 'y' || play_again === 'n') {
                                 if(play_again === 'y') {
                                     document.querySelector('.coin-toss-results').innerHTML = "";
-                                    document.querySelector('.play-again').classList.remove('active');
+                                    document.querySelector('.coin-play-again').classList.remove('active');
                                     headsOrTails(name);
                                 } else {
                                     document.querySelector('#coin-toss').removeAttribute('class');
@@ -209,12 +301,152 @@
 
     // ******************** Guess Which Hand / Left or Right ********************
     function leftOrRight(name) {
+        /*
+        * Add active class to "Guess Which Hand"
+        * make sure input is empty and focus on input
+        */
+        document.querySelector('#guess-which-hand').setAttribute('class', 'active');
+        document.querySelector('#hand-option').value = "";
+        document.querySelector('.which-hand-results').innerHTML = "";
+        document.querySelector('.hand-play-again').classList.remove('active');
+        document.querySelector('#which-hand-again').value = "";
+        document.querySelector('#hand-option').focus();
 
+        document.querySelector('#hand-option').addEventListener('keyup', function(e) {
+            var hand_option = document.querySelector('#hand-option').value.toLowerCase();
+
+            if(e.key === 'Enter') {
+                e.preventDefault();
+
+                if(hand_option === 'left' || hand_option === 'right') {
+                    whichHand(hand_option, name);
+                } else {
+                    document.querySelector('#hand-option').value = "";
+                    document.querySelector('#hand-option').focus();
+                }
+            }
+        });
+
+        function whichHand(hand, name) {
+            var whichHand = Math.floor(Math.random() * 2);
+
+            if(whichHand === 1) {
+                whichHand = 'left';
+            } else {
+                whichHand = 'right';
+            }
+
+            switch(name.toLowerCase()) {
+                case 'clark':
+                    break;
+                case 'wopr':
+                    break;
+                default:
+                    if(hand === whichHand) {
+                        document.querySelector('.which-hand-results').innerHTML = "It's " + hand + "! You win!";
+                    } else {
+                        document.querySelector('.which-hand-results').innerHTML = "Sorry, it's " + whichHand + ". You lose.";
+                    }
+
+                    document.querySelector('.hand-play-again').classList.add('active');
+                    document.querySelector('#which-hand-again').value = "";
+                    document.querySelector('#which-hand-again').focus();
+
+                    document.querySelector('#which-hand-again').addEventListener('keyup', function(e) {
+                        var play_again = document.querySelector('#which-hand-again').value.toLowerCase();
+
+                        if(e.key === 'Enter') {
+                            e.preventDefault();
+
+                            if(play_again === 'y' || play_again === 'n') {
+                                if(play_again === 'y') {
+                                    document.querySelector('.which-hand-results').innerHTML = "";
+                                    document.querySelector('.hand-play-again').classList.remove('active');
+                                    leftOrRight(name);
+                                } else {
+                                    document.querySelector('#guess-which-hand').removeAttribute('class');
+                                    showGamesList(name);
+                                }
+                            }   else {
+                                document.querySelector('#which-hand-again').value = "";
+                                document.querySelector('#which-hand-again').focus();
+                            }
+                        }
+                    });
+            }
+        }
     }
 
     // ******************** Pick a Number Between 1 and 10 ********************
     function oneToTen(name) {
+        /*
+        * Add active class to "Pick a Number"
+        * make sure input is empty and focus on input
+        */
+        document.querySelector('#pick-a-number').setAttribute('class', 'active');
+        document.querySelector('#one-ten-option').value = "";
+        document.querySelector('.one-ten-results').innerHTML = "";
+        document.querySelector('.one-ten-play-again').classList.remove('active');
+        document.querySelector('#one-ten-again').value = "";
+        document.querySelector('#one-ten-option').focus();
 
+        document.querySelector('#one-ten-option').addEventListener('keyup', function(e) {
+            var one_ten_option = parseFloat(document.querySelector('#one-ten-option').value);
+
+            if(e.key === 'Enter') {
+                e.preventDefault();
+
+                if(!isNaN(one_ten_option) && (one_ten_option >= 1 && one_ten_option <= 10)) {
+                    oneTen(one_ten_option, name);
+                } else {
+                    document.querySelector('#one-ten-option').value = "";
+                    document.querySelector('#one-ten-option').focus();
+                }
+            }
+        });
+
+        function oneTen(num, name) {
+            var randomNum = Math.floor((Math.random() * 10) + 1);
+
+            switch(name.toLowerCase()) {
+                case 'clark':
+                    break;
+                case 'wopr':
+                    break;
+                default:
+                    if(num === randomNum) {
+                        document.querySelector('.one-ten-results').innerHTML = "It's " + num + "! You win!";
+                    } else {
+                        document.querySelector('.one-ten-results').innerHTML = "Sorry, it's " + randomNum + ". You lose.";
+                    }
+
+                    document.querySelector('.one-ten-play-again').classList.add('active');
+                    document.querySelector('#one-ten-again').value = "";
+                    document.querySelector('#one-ten-again').focus();
+
+                    document.querySelector('#one-ten-again').addEventListener('keyup', function(e) {
+                        var play_again = document.querySelector('#one-ten-again').value.toLowerCase();
+
+                        if(e.key === 'Enter') {
+                            e.preventDefault();
+
+                            if(play_again === 'y' || play_again === 'n') {
+                                if(play_again === 'y') {
+                                    document.querySelector('.one-ten-results').innerHTML = "";
+                                    document.querySelector('.one-ten-play-again').classList.remove('active');
+                                    oneToTen(name);
+                                } else {
+                                    document.querySelector('#pick-a-number').removeAttribute('class');
+                                    showGamesList(name);
+                                }
+                            }   else {
+                                document.querySelector('#one-ten-again').value = "";
+                                document.querySelector('#one-ten-again').focus();
+                            }
+                        }
+                    });
+            }
+        }
     }
 
     // ******************** Global Thermonuclear War ********************
